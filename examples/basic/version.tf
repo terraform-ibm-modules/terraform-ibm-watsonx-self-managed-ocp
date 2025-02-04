6133834732b9ce97daf1c1cdf4cffe4da25e3c60
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2025-02-04 15:04:55 +0000

    SKIP UPGRADE TESTS

--- examples/basic/version.tf
+++ examples/basic/version.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.3"
   required_providers {
-    # renovate is set up to keep provider version at the latest for all DA solutions
+    # renovate is set up to keep provider version at the latest for all DA solutions.
     ibm = {
       source  = "IBM-Cloud/ibm"
       version = "1.71.3"
