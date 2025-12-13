db7c9ac3c1025208a40b6e3256cc64111ae4ef6f
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-13 08:28:04 +0000

    fix(deps): update dependencies

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.85.0"
+      version = "1.86.1"
     }
     external = {
       source  = "hashicorp/external"
